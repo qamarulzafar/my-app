21333edb2d3f7856e13b6f05ff4e791530d6d9b0
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2024-11-11 04:57:52 -0700

    alignment

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,9 +5,9 @@ import bg from '../images/bg.jpg'
 const Hero = () => {
   return (
     <div>
-      <div className="hero relative">
-        <Image src={bg} alt="Hero-img"  className="w-full absolute h-screen"   />
-        <div className="center relative flex justify-center flex-col text-white items-center h-screen">
+      <div className="hero relative grid grid-rows-1">
+        <Image src={bg} alt="Hero-img"  className="w-full absolute h-full  object-cover"   />
+        <div className="center relative  justify-center flex-col text-white items-center py-10 md:py-24 grid place-items-center h-full">
             <div className="head">
                 <h2 className="text-4xl w-3/4 sm:text-5xl md:text-6xl md:w-1/2 text-center font m-auto font-bold">Virtual Assistant 
                 at Your Service</h2>
